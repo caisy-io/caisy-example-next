b78b5161508cc4398c5be5794a32a0fea276b510
Moritz Gruber <m.gruber@cyclus.digital>
2023-02-11 19:10:12 +0100

    chore: update index page
    chore: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,8 +13,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-          <Link href="/blog/my-first-article">a link to our first blog page</Link>
+        <div className={styles.description}>
+          <Link href="/blog/my-first-article">
+              a link to our first blog page
+          </Link>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+          <div className={styles.thirteen}>
+            <Image
+              src="/thirteen.svg"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+          </div>
+        </div>
       </main>
     </>
-  )
+  );
 }
